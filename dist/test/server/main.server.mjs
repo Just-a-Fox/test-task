bf13b8f407bd7fb79950ac8651853275190145d2
Ivan Fox <just.a.fox.2003@gmail.com>
2024-03-18 18:30:51 +0100

    test-task v1.3.1

--- dist/test/server/main.server.mjs
+++ dist/test/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-F57YKG44.mjs";import"./chunk-XXZTFEVW.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-CR7V64NW.mjs";import"./chunk-PFHXSCM6.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
